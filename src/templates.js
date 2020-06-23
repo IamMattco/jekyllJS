54eca26e97ebd0f7e3fa47546a9c65b05fc439de
Mateusz Mroz <mattowskycom@gmail.com>
2020-06-23 14:30:39 +0200

    move tempates to separate file #1

--- src/templates.js
+++ src/templates.js
@@ -8,6 +8,7 @@ let header = `
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="${config.blogDescription}" />
       <title>${config.blogTitle}</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   </head>
   <body>
 `
@@ -21,6 +22,21 @@ let footer = `
   </body>
 </html>
 `
+
+const posthtml = data => `
+${header}
+  <header>
+    <a href="/">Go back home</a>
+  </header>
+  <div class="post-content">
+    <img src="${data.attributes.thumbnail}" />
+    <h1>${data.attributes.title}</h1>
+    <p>dodane przez ${data.attributes.author}, ${data.attributes.date}</p>
+    <hr />
+    ${data.body}
+  </div>
+${footer}
+`;
 
 const homepage = posts => `
 ${header}
@@ -39,7 +55,7 @@ config.authorTwitter
           ${posts
             .map(
               post => `<div class="post">
-              <h3><a href="./${post.path}">${
+              <h3><a href="/posts/${post.path}">${
                 post.attributes.title
               }</a></h3>
                   <small>${new Date(
@@ -54,6 +70,20 @@ config.authorTwitter
 ${footer}
 `;
 
+const pagehtml = data => `
+${header}
+  <header>
+    <a href="/">Go back home</a>
+  </header>
+  <div class="post-content">
+    <h1>${data.attributes.title}</h1>
+    ${data.body}
+  </div>
+${footer}
+`;
+
 module.exports = {
-  homepage: homepage
+  homepage: homepage,
+  posthtml: posthtml,
+  pagehtml: pagehtml
 };
